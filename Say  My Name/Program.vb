02d39424352f25be238ce0ebde1c1848835cb097
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-17 08:26:06 -0700

    This is the intitial code

--- Say  My Name/Program.vb
+++ Say  My Name/Program.vb
@@ -2,6 +2,8 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Hello operator, what is your name?")
+        Dim UsrName As String = Console.ReadLine()
+        Console.WriteLine($"Welcome, {UsrName}!")
     End Sub
 End Module
